4d8441416dd94eef3e90da31cc2e60a142663034
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-12 13:27:25 +0300

    Create Container component

---
+++ components/elements/container/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Index({ children })
+{
+    return (
+        <div className='w-5/6 mx-auto mt-10'>
+            {children}
+        </div>
+    )
+}
+
+export default Index
